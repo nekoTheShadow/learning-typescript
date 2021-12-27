6b60bfc9ed491b0521438549aabe8c513e9a36e6
hajime nakamura <h.nakamura0903@gmail.com>
2021-12-27 21:01:07 +0900

    SECTION-012: 「バリデーションの追加」まで

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,3 +1,65 @@
+class HitAndBlow {
+    private readonly answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    private answer: string[] = []
+    private tryCount = 0
+
+    async play() {
+        const inputArr = (await promptInput("「,」区切りで3つの数字を入力してください")).split(',')
+
+        if (!this.validate(inputArr)) {
+            printLine("無効な入力です")
+            await this.play()
+            return 
+        }
+
+        const result = this.check(inputArr)
+        if (result.hit != this.answer.length) {
+            printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`)
+            this.tryCount++
+            await this.play()
+        } else {
+            this.tryCount++
+        }
+    }
+
+    setting() {
+        const answerLength = 3
+
+        while (this.answer.length < answerLength) {
+            const randNum = Math.floor(Math.random() * this.answerSource.length)
+            const selectedItem = this.answerSource[randNum]
+            if (!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem)
+            }
+        }
+    }
+
+    check(input: string[]) {
+        let hitCount = 0
+        let blowCount = 0
+        input.forEach((val, index) => {
+            if (val === this.answer[index]) {
+                hitCount++
+            } else if (this.answer.includes(val)) {
+                blowCount++;
+            }
+        })
+        return {hit: hitCount, blow: blowCount}
+    }
+
+    end() {
+        printLine(`正解です!\n試行回数: ${this.tryCount}回`)
+        process.exit()
+    }
+
+    private validate(inputArr: string[]) {
+        const isLengthValid = inputArr.length === this.answer.length
+        const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
+        const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) === i)
+        return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
+    }
+}
+
 const printLine = (text: string, breakLine: boolean = true) => {
     process.stdout.write(text + (breakLine ? "\n" : ""))
 }
@@ -11,9 +73,8 @@ const promptInput = async (text: string) => {
 }
 
 (async () => {
-    const name = await promptInput("名前を入力してください")
-    console.log(name)
-    const age = await promptInput("年齢を入力してください")
-    console.log(age)
-    process.exit()
+    const hitAndBlow = new HitAndBlow()
+    hitAndBlow.setting()
+    await hitAndBlow.play()
+    hitAndBlow.end()
 })()
