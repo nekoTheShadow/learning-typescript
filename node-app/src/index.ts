d6342fe1fd971c3a2e49a10557f9cb4be20abcb5
hajime nakamura <h.nakamura0903@gmail.com>
2021-12-27 20:34:11 +0900

    SECTION-011

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,6 +1,19 @@
-const sayHello = (name: string) => {
-    return `Hello ${name}!`
+const printLine = (text: string, breakLine: boolean = true) => {
+    process.stdout.write(text + (breakLine ? "\n" : ""))
 }
 
-// console.log(sayHello('Micheal Jackson'))
-process.stdout.write(sayHello('Micheal Jackson'))
+const promptInput = async (text: string) => {
+    printLine(`\n${text}\n>`, false)
+    const input: string = await new Promise((resolve) => {
+        process.stdin.once('data', (data) => resolve(data.toString()))
+    })
+    return input.trim()
+}
+
+(async () => {
+    const name = await promptInput("名前を入力してください")
+    console.log(name)
+    const age = await promptInput("年齢を入力してください")
+    console.log(age)
+    process.exit()
+})()
