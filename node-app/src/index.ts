15839069a4f8d569ed6325d01dc023d1b4b5bf3b
hajime nakamura <h.nakamura0903@gmail.com>
2021-12-29 21:46:35 +0900

    ゲームの変更機能の実装

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,4 +1,4 @@
-const nextActions = ['play again', 'exit'] as const
+const nextActions = ['play again', 'exit', 'change game'] as const
 type NextAction = typeof nextActions[number]
 
 type GameStore = {
@@ -35,6 +35,9 @@ class GameProcedure {
             await this.play()
         } else if (action === 'exit') {
             this.end()
+        } else if (action === 'change game') {
+            await this.select()
+            await this.play()
         } else {
             const neverValue: never = action
             throw new Error(`${neverValue} is an invalid action.`)
